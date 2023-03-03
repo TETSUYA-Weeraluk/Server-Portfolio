1de1cba2b53e1ec78a6de6535e8d1fcecf011623
TETSUYA <weeraluk.sopa@gmail.com>
2023-03-04 03:21:29 +0700

    feat : function create user

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-const express = require('express');
+const express = require('express')
+const authRouter = require('./routes/auth')
+const app = express()
+const cors = require('cors')
+const port = 7777
+
+app.use(express.json())
+app.use(cors());
+app.use('/api/users',authRouter)
+    
+app.listen(port, () => {
+    console.log(`Server app listening on port ${port}`)
+})
---
+++ controller/auth.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const {connection} = require('../db');
+
+const register = (req, res) => {
+  // เช็ค username , email
+  const q = "SELECT * FROM users WHERE username = ? OR email = ?";
+
+  connection.execute(q, [req.body.username, req.body.email], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length) return res.status(409).json("User already exists!");
+
+    //hash the password
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    //Insert to database
+    const date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    const q =
+    `INSERT INTO users (username,fname,lname,email,password, id_role,create_time) \
+       VALUES ('${req.body.username}','${hash}', '${req.body.fname}', '${req.body.lname}','${req.body.email}','1','${date}')`;
+
+    connection.execute(q,(err,data) => {
+        if (err) return res.json(err);
+        return res.status(200).json('User has been created')
+    })
+  });
+};
+
+
+
+module.exports = {
+  register,
+}
