57fc1d2a24cfc4269f88bc8fa251b31aa239ce29
Weeraluk Sopapan <weeraluk.sopa@gmail.com>
2023-03-23 19:47:53 +0700

    update : insertData

--- controller/dataMangement.js
+++ controller/dataMangement.js
@@ -113,7 +113,81 @@ const updateDataManagement = (req, res) => {
   });
 };
 
+const insertDataManagement = (req, res) => {
+  if (req.body.table === "") {
+    console.log("ค่าว่าง");
+    return;
+  } else {
+    const table = req.body.table;
+    const column = req.body.column;
+    const value = req.body.value;
+    const q = `INSERT INTO ${table} (${column}) VALUES (${value});`;
+    console.log(q);
+    try {
+      connection.execute(q, (err, data) => {
+        if (err) return res.json(err);
+        res.json("success");
+      });
+    } catch (error) {
+      res.send({ error: error });
+    }
+  }
+};
+
+const selectItemSkill = (req, res) => {
+  if (req.body.table === "") {
+    console.log("ค่าว่าง");
+    return;
+  } else {
+    const q = `SELECT * FROM skill_item`;
+    try {
+      connection.execute(q, (err, data) => {
+        if (err) return res.json(err);
+        res.json(data);
+      });
+    } catch (error) {
+      res.send({ error: error });
+    }
+  }
+};
+
+const insertItemSkill = (req, res) => {
+  const id_users = req.body.value[0];
+  const title = req.body.value[1];
+  const imgSkill = req.body.value[2];
+  const q_check_Title = `SELECT * FROM skill_item WHERE title = ${title}`;
+  const q_insert = `INSERT INTO skill_item (title,icons_skill) VALUES (${title},${imgSkill})`;
+  const q_select_lastInsert = `SELECT id_skill_item FROM skill_item WHERE title = ${title}`;
+  const q_insert_itemSkill_users = `INSERT INTO skill (id_skill_item,id_users) VALUES (?,${id_users})`;
+  try {
+    connection.execute(q_check_Title, (err, data) => {
+      if (err) return res.json(err);
+      if (data.length) return res.status(409).json("Title already exists!");
+
+      connection.execute(q_insert, (err, data) => {
+        if (err) return res.json(err);
+        connection.execute(q_select_lastInsert, (err, data) => {
+          if (err) return res.json(err);
+          connection.execute(
+            q_insert_itemSkill_users,
+            [data[0].id_skill_item],
+            (err, data) => {
+              if (err) return res.json(err);
+              res.json("Success");
+            }
+          );
+        });
+      });
+    });
+  } catch (error) {
+    res.json(error);
+  }
+};
+
 module.exports = {
   getAllDataManagement,
   updateDataManagement,
+  insertDataManagement,
+  selectItemSkill,
+  insertItemSkill,
 };
--- routes/dataManagement.js
+++ routes/dataManagement.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { getAllDataManagement, updateDataManagement } = require('../controller/dataMangement');
+const { getAllDataManagement, updateDataManagement, insertDataManagement, selectItemSkill, insertItemSkill } = require('../controller/dataMangement');
 const router = express.Router();
 
 router.get('/getalldatamanagement', getAllDataManagement)
+router.get('/selectItemSkill', selectItemSkill)
 router.post('/updatedatamanagement', updateDataManagement)
+router.post('/insertdatamanagement', insertDataManagement)
+router.post('/insertItemSkill', insertItemSkill)
 
 module.exports = router;
